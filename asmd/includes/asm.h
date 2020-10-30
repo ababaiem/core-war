f4a3d56f8945856a72faa361d48c0d8618de23a9
Alborz Babaie Mouziraji <ababaie-@student.s19.be>
2020-10-30 10:31:25 +0100

    Included stdint for linux

--- asmd/includes/asm.h
+++ asmd/includes/asm.h
@@ -6,13 +6,17 @@
 /*   By: ababaie- <ababaie-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/29 18:08:49 by ababaie-          #+#    #+#             */
-/*   Updated: 2020/10/30 10:28:39 by ababaie-         ###   ########.fr       */
+/*   Updated: 2020/10/30 10:30:51 by ababaie-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASM_H
 
 # define ASM_H
+
+#if defined(__linux__)
+	# include <stdint.h>
+#endif
 
 # include "libft.h"
 # include "op.h"
